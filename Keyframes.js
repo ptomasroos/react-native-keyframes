5f4823bce5c22273e644f75f85fb1e0818a2840d
Tomas Roos <ptomasroos@gmail.com>
2017-04-05 10:28:15 +0200

    Imperative js api

--- Keyframes.js
+++ Keyframes.js
@@ -1,11 +1,45 @@
 import React, { PropTypes } from 'react';
-import { NativeModules, requireNativeComponent, View } from 'react-native';
+import {
+  NativeModules,
+  requireNativeComponent,
+  View,
+  UIManager,
+  findNodeHandle
+} from 'react-native';
 
 const PTRKeyframesView = requireNativeComponent('PTRKeyframesView', Keyframes);
 
 class Keyframes extends React.Component {
   render() {
-    return <PTRKeyframesView {...this.props} />;
+    return <PTRKeyframesView ref={ref => this.ref = ref} {...this.props} />;
+  }
+
+  seek(position) {
+    this.runCommand('seek', [position]);
+  }
+
+  setRepeatCount(count) {
+    this.runCommand('setRepeatCount', [count]);
+  }
+
+  start() {
+    this.runCommand('start');
+  }
+
+  stop() {
+    this.runCommand('stop');
+  }
+
+  runCommand(name, args = []) {
+    UIManager.dispatchViewManagerCommand(
+      this.getHandle(),
+      UIManager.PTRKeyframesView.Commands[name],
+      args
+    );
+  }
+
+  getHandle() {
+    return findNodeHandle(this.ref);
   }
 }
 
